6d7619e3b76bdeeacaf35c095d1020051654fffe
Fransheska <fransheskavalenzuela@gmail.com>
2024-04-04 11:43:53 -0300

    commit 2

---
+++ Palindromos/Palindromos.java
@@ -0,0 +1,16 @@
+package Palindromos;
+
+public class Palindromos{
+    String cadena;
+    Palindromos(){
+
+        this.cadena = cadena;
+    }
+
+    private static String[] separaStrings(String cadena){
+        String[] cadenaArray= cadena.split("\\ ");
+        return cadenaArray;
+
+    }
+    
+}
